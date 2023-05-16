1db231b14175bf761f80339b3568202c517a055c
luteranlajos@protonmail.ch <luteranlajos@protonmail.ch>
2023-05-16 15:29:21 +0200

    add minimum required Linux kernel version

--- src/blocks/memory.rs
+++ src/blocks/memory.rs
@@ -34,10 +34,10 @@
 //! `swap_free_percents`      | as above but as a percentage of total memory                                    | Number | Percents
 //! `swap_used`               | Swap used                                                                       | Number | Bytes
 //! `swap_used_percents`      | as above but as a percentage of total memory                                    | Number | Percents
-//! `zswap_compressed`        | Compressed zswap memory usage                                                   | Number | Bytes
-//! `zswap_decompressed`      | Decompressed zswap memory usage                                                 | Number | Bytes
-//! `zswap_decompressed_percents` | as above but as a percentage of total zswap memory                          | Number | Percents
-//! 'zswap_comp_ratio'        | Ratio of the decompressed/compressed zswap memory                               | Number | -
+//! `zswap_compressed`        | Compressed zswap memory usage (>=Linux 5.19)                                    | Number | Bytes
+//! `zswap_decompressed`      | Decompressed zswap memory usage (>=Linux 5.19)                                  | Number | Bytes
+//! `zswap_decompressed_percents` | as above but as a percentage of total zswap memory  (>=Linux 5.19)          | Number | Percents
+//! 'zswap_comp_ratio'        | Ratio of the decompressed/compressed zswap memory (>=Linux 5.19)                | Number | -
 //!
 //! Action          | Description                               | Default button
 //! ----------------|-------------------------------------------|---------------
