28acdec48ba7305c4cf05d116b6503f88ea49066
Luterán Lajos <luteranlajos@protonmail.ch>
2024-09-18 14:59:39 +0200

    lali: fix install.sh
    Signed-off-by: Luterán Lajos <luteranlajos@protonmail.ch>

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 set -ex
 
 cargo zigbuild --profile release-small --locked
-cp target/release-small/i3status-rs ~/.local/bin/cargo/bin/i3status-rs
+cp -f target/release-small/i3status-rs ~/.local/bin/cargo/bin/i3status-rs
